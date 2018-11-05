918ff9c5fc022c524e73bbd8b6fc5b17258108d5
Nicole D <nnardella@gmail.com>
2018-11-05 18:00:39 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,10 +6,10 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
-    if BRANDS.include?(brand)
+    BRANDS.include?(brand) BRANDS << brand : nil
       nil
     else
-      BRANDS << brand
+      
    end
   end
 
