504f992ccf5e2b03538b446d1c70cf08c9f0ae62
Nicole D <nnardella@gmail.com>
2018-11-05 17:59:24 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,11 +6,11 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
-    if BRANDS.include?(brand) 
+    if BRANDS.include?(brand)
       nil
     else
       BRANDS << brand
-
+   end
   end
 
   def cobble
